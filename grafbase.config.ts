34fd0e9ead07e37f9849e11ae1dbbba17135ecdf
arzukarla2 <65809748+arzukarla2@users.noreply.github.com>
2024-02-01 18:02:06 +0000

    second  commit

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,5 +1,8 @@
-import { graph, config } from '@grafbase/sdk'
+import { graph, config, auth } from '@grafbase/sdk'
 
+
+const g = graph.Standalone()
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
@@ -9,7 +12,7 @@ const User = g.model('User', {
   linkedinUrl: g.url().optional(), 
   projects: g.relation(() => Project).list().optional(),
 })
-
+// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({ min: 3 }),
   description: g.string(), 
@@ -18,4 +21,7 @@ const Project = g.model('Project', {
   githubUrl: g.url(), 
   category: g.string().search(),
   createdBy: g.relation(() => User),
+})
+export default config({
+  graph: g
 })
